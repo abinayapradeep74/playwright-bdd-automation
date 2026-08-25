03f3fa3135ae7efd1802779d73cfcd722cdcb56c
Abinaya Maruthamuthu <abinayapradeep74@gmail.com>
2026-08-25 13:08:30 +0100

    Add environment configuration

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,8 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
+import dotenv from 'dotenv';
+dotenv.config();
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -31,12 +33,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   
   
   use: {
-    baseURL:"https://opensource-demo.orangehrmlive.com",
+    baseURL:process.env.BASE_URL,
 
     trace: 'on-first-retry',
         screenshot: 'only-on-failure',
